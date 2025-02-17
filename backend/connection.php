4e320e539f278655e92acafc749ee710a92c16c0
jcister <joshuacister9@gmail.com>
2025-02-17 15:29:29 +0800

    Login Page and Create User Page UI

--- backend/connection.php
+++ backend/connection.php
@@ -2,7 +2,7 @@
     $host = "localhost";
     $user = "root";
     $password = "";
-    $database = "bini_academy_db";
+    $database = "bini_acdemy_db";
 
     // Create connection
     $conn = mysqli_connect($host, $user, $password, $database);
