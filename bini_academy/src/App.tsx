a62c304379851e8824364fc7eb65c89e072673a6
YuichiCanete <ycanete_220000000743@uic.edu.ph>
2025-02-22 23:44:18 +0800

    Add: Mantine Notfications

--- bini_academy/src/App.tsx
+++ bini_academy/src/App.tsx
@@ -1,14 +1,16 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { LoginPage } from './pages/login';
-import { UserCreatePage} from './pages/create';
+import { UserCreatePage } from './pages/create';
 // import { HomePage} from './pages/homepage';
 
 import '@mantine/core/styles.css'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import { MantineProvider } from '@mantine/core';
+import { Notifications } from '@mantine/notifications';
 import { MainTest } from './test/main';
+import '@mantine/notifications/styles.css';
 
 export const queryClient = new QueryClient();
 
@@ -17,12 +19,13 @@ function App() {
     <>
       <QueryClientProvider client={queryClient}>
         <MantineProvider>
+        <Notifications position='top-right' autoClose={1500} limit={3}/>
           <Router>
             <Routes>
               {/* <Route path="/" element={<HomePage/>} /> */}
-              <Route path="/" element={<LoginPage/>} />
-              <Route path='/test' element={<MainTest/>}></Route>
-              <Route path='/usercreate' element={<UserCreatePage/>}></Route>
+              <Route path="/" element={<LoginPage />} />
+              <Route path='/test' element={<MainTest />}></Route>
+              <Route path='/usercreate' element={<UserCreatePage />}></Route>
             </Routes>
           </Router>
         </MantineProvider>
