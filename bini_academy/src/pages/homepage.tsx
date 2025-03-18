7184c7dddc7f2aa5643456efdb2ee1da7da6864f
YuichiCanete <ycanete_220000000743@uic.edu.ph>
2025-03-18 23:19:53 +0800

    Update: Hopefully more readable code

--- bini_academy/src/pages/homepage.tsx
+++ bini_academy/src/pages/homepage.tsx
@@ -1,11 +1,11 @@
-import { Container, SimpleGrid, Button, Text, Title, Grid, Image, Card } from "@mantine/core";
+import { Container, SimpleGrid, Button, Text, Title, Grid, Image } from "@mantine/core";
 import { CategoryCard } from '../components/categorycard';
 import { CourseCard } from '../components/coursecard';
 import bapaLogo from "../assets/bapalogo.svg";
 import { HomeHeader } from "../components/homeheader";
 import { Course, CourseCategory, CourseCategoryLabel } from "../types";
 import { getCourseCount, listCourse } from "../api/course";
-import { useEffect, useState } from "react";
+import { ReactNode, useEffect, useState } from "react";
 import { Loading } from "../components/loading";
 import { IconBook } from "@tabler/icons-react";
 import { Footer } from "../components/footer";
@@ -13,14 +13,15 @@ import placeholderImg from '../assets/placeholder-image.svg'
 import { listEvents } from "../api/event";
 import { ProgramEvent } from "../types";
 
-
 export const HomePage = () => {
+
     const [isCourseCountLoading, setCourseCountLoading] = useState<boolean>(false);
     const [courseCount, setCourseCount] = useState<{ program_category: CourseCategory, course_count: number }[] | null>(null);
 
     const [isCoursesLoading, setCoursesLoading] = useState<boolean>(false);
     const [courses, setCourses] = useState<Course[] | null>(null)
 
+    const [isEventsLoading, setEventsLoading] = useState<boolean>(false)
     const [events, setEvents] = useState<ProgramEvent[] | null>(null);
 
     const fetchCourseCount = async () => {
@@ -53,12 +54,15 @@ export const HomePage = () => {
 
     const fetchEvents = async () => {
         try {
+            setEventsLoading(true)
             await new Promise(resolve => setTimeout(resolve, 5000));
             const response = await listEvents();
             console.table(response)
             setEvents(response)
         } catch (error) {
             console.log('Error BUllshit')
+        } finally {
+            setEventsLoading(false)
         }
     }
 
@@ -100,68 +104,46 @@ export const HomePage = () => {
                     </Grid>
                 </div>
 
-                {/* Browse Top Categories */}
-                {courseCount && courseCount.length > 0 && <Title order={2} className="text-xl font-semibold text-center mt-10">
-                    Programs Category
-                </Title>}
+                <GridComponent title={"Programs Category"} loadingState={isCourseCountLoading}>
+                    {courseCount && courseCount.length > 0 &&
+                        courseCount.map((course) => {
+                            const categoryData = CourseCategoryLabel.find(
+                                (c) => c.value === course.program_category
+                            );
 
-                {isCourseCountLoading ? (
-                    <Loading width={64} height={64} />
-                ) : (
-                    <SimpleGrid cols={3} spacing="lg" className="mt-5">
-                        {courseCount && courseCount.length > 0 &&
-                            courseCount.map((course) => {
-                                const categoryData = CourseCategoryLabel.find(
-                                    (c) => c.value === course.program_category
-                                );
+                            if (categoryData) {
+                                return <CategoryCard
+                                    key={course.program_category}
+                                    title={categoryData.label}
+                                    courses={course.course_count}
+                                    icon={<categoryData.icon size={30} />}
+                                />
+                            } else {
+                                return <CategoryCard
+                                    key={course.program_category}
+                                    title={course.program_category}
+                                    courses={course.course_count}
+                                    icon={<IconBook size={30} />}
+                                />
+                            }
+                        })
+                    }
+                </GridComponent>
 
-                                if (categoryData) {
-                                    return <CategoryCard
-                                        key={course.program_category}
-                                        title={categoryData.label}
-                                        courses={course.course_count}
-                                        icon={<categoryData.icon size={30} />}
-                                    />
-                                } else {
-                                    return <CategoryCard
-                                        key={course.program_category}
-                                        title={course.program_category}
-                                        courses={course.course_count}
-                                        icon={<IconBook size={30} />}
-                                    />
-                                }
-                            })
-                        }
-                    </SimpleGrid>
-                )}
+                <GridComponent loadingState={isCoursesLoading} title="Best Selling Courses">
+                    {courses && courses.length > 0 && courses.map((course, key) => (
+                        <CourseCard
+                            key={key}
+                            image={placeholderImg}
+                            title={course.course_title}
+                            category={course.course_topic}
+                            price={`₱${new Intl.NumberFormat("en-PH").format(Number(course.price))}`}
+                            students={"100"}
+                        />
+                    ))}
+                </GridComponent>
 
-                {/* Best Selling Courses */}
-                {courses && courses.length > 0 && <Title order={2} className="text-xl font-semibold text-center mt-10">
-                    Best Selling Courses
-                </Title>}
-
-                {isCoursesLoading ? (
-                    <Loading width={64} height={64} />
-                ) : (
-                    <SimpleGrid cols={3} spacing="lg" className="mt-5">
-                        {courses && courses.length > 0 && courses.map((course, key) => (
-                            <CourseCard
-                                key={key}
-                                image={placeholderImg}
-                                title={course.course_title}
-                                category={course.course_topic}
-                                price={`₱${new Intl.NumberFormat("en-PH").format(Number(course.price))}`}
-                                students={"100"}
-                            />
-                        ))}
-                    </SimpleGrid>
-                )}
-
-                <Title order={2} className="text-xl font-semibold text-center mt-10">
-                    Events
-                </Title>
-
-                <SimpleGrid cols={3} spacing="lg" className="mt-5">
+                <GridComponent title={"Events"} loadingState={isEventsLoading}>
                     {events && events.map((ee, key) => {
                         return (
                             <CourseCard
@@ -172,12 +154,28 @@ export const HomePage = () => {
                             />
                         );
                     })}
-                </SimpleGrid>
-
-
+                </GridComponent>
 
             </Container>
             <Footer />
         </>
+    );
+};
+
+interface GridProps {
+    title: string;
+    loadingState: boolean;
+    children: ReactNode;
+}
+
+// Custom Component for grouping title, loading and fetched data
+export const GridComponent = ({ loadingState, children, title }: GridProps) => {
+    return (
+        <div>
+            <Title order={2} className="text-xl font-semibold text-center mt-10">
+                {title}
+            </Title>
+            {loadingState ? <Loading /> : <SimpleGrid cols={3} spacing="lg" className="mt-5">{children}</SimpleGrid>}
+        </div>
     );
 };
