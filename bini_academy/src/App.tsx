fb85283ee38d61dbc3c47896eac77a71ef6aa28d
YuichiCanete <ycanete_220000000743@uic.edu.ph>
2025-03-21 22:19:54 +0800

    Add: query of the tanstack

--- bini_academy/src/App.tsx
+++ bini_academy/src/App.tsx
@@ -12,6 +12,10 @@ import '@mantine/core/styles.css'
 import { MantineProvider } from '@mantine/core';
 import { Notifications } from '@mantine/notifications';
 import '@mantine/notifications/styles.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient()
+
 
 function App() {
 
@@ -19,17 +23,19 @@ function App() {
     <>
       <MantineProvider >
         <Notifications position='top-right' autoClose={1500} limit={3} />
-        <Router>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path='/usercreate' element={<UserCreatePage />}></Route>
-            <Route path='/homepage' element={<HomePage />}></Route>
-            <Route path='/instructorcreatecourse' element={<InstructorCreateCourse />}> </Route>
-            <Route path='/usermanage' element={<ManageUsers />}></Route>
-            <Route path='/hostevents' element={<HostEvents />}></Route>
-          </Routes>
-        </Router>
+        <QueryClientProvider client={queryClient}>
+          <Router>
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/login" element={<LoginPage />} />
+              <Route path='/usercreate' element={<UserCreatePage />}></Route>
+              <Route path='/homepage' element={<HomePage />}></Route>
+              <Route path='/instructorcreatecourse' element={<InstructorCreateCourse />}> </Route>
+              <Route path='/usermanage' element={<ManageUsers />}></Route>
+              <Route path='/hostevents' element={<HostEvents />}></Route>
+            </Routes>
+          </Router>
+        </QueryClientProvider>
       </MantineProvider>
     </>
   );
