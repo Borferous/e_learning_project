18e372be41ab9e274b53988c0740bd71c926af29
YuichiCanete <ycanete_220000000743@uic.edu.ph>
2025-04-06 00:09:47 +0800

    Minor fixes

--- bini_academy/src/pages/payment_information.tsx
+++ bini_academy/src/pages/payment_information.tsx
@@ -1,6 +1,6 @@
 import { Header } from "../components/header"
 import { Footer } from "../components/footer"
-import { Card, Grid, NumberInput, Select, Text, Center } from "@mantine/core"
+import { Card, Grid, NumberInput, Select, Text, Center, Checkbox } from "@mantine/core"
 import { Button } from "@mantine/core"
 import { IconArrowLeft } from "@tabler/icons-react"
 import { IconCloudDownload } from "@tabler/icons-react"
@@ -56,18 +56,27 @@ export const PaymentInformation = () => {
                                 </Button>
                             </div>
                         </Grid.Col>
-
                     </Grid>
 
                     <Center>
                         <Button
                             color="orange"
+                            disabled={!paymentForm.values.agreedToTerms}
                         >
                             Submit
                         </Button>
                     </Center>
-                    <p>By Checking the box, you acknowledge that you have read, understood, and agreed to the <span className="text-orange-500" onClick={() => navigate('/terms-and-conditions')}>terms and conditions</span></p>
 
+                    <Checkbox
+                        defaultChecked={false}
+                        onChange={(event) => paymentForm.setFieldValue('agreedToTerms', event.currentTarget.checked)}
+                        label={
+                            <>
+                                By Checking the box, you acknowledge that you have read, understood, and agreed to the 
+                                <span className="text-orange-500" onClick={() => navigate('/terms-and-conditions')}> terms and conditions</span>
+                            </>
+                        }
+                    />
                 </Card>
             </Center>
             <Footer />
